2b83447a019be301ad502fedf88dee79d60aed62
kevin <kevin_365@live.com>
2022-09-13 19:33:55 +0800

    2022-09-13

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import os
 import hashlib
 import pandas as pd
 
-base_dir = r'D:\mate20\mate20音乐'  # 需要查找的根目录
+base_dir = r'F:\手机文件整理\mate20\mate20音乐'  # 需要查找的根目录
 save_path = r'C:\Users\Kevin\Documents\文件信息.xlsx'  # 结果保存
 df = pd.DataFrame(columns=('长文件名', '文件名', '后缀', '大小', 'MD5'))
 
@@ -28,12 +28,8 @@ for roots, dirs, file_names in os.walk(base_dir):
         df.loc[file_num] = file_name, name, file_suf, file_size, file_md5
         file_num += 1
 
-song = df['文件名'].str.split('-')
-df['歌手'] = song.str[0].str.strip()
-df['歌名'] = song.str[1].str.strip()
-df.sort_values(by='歌名', ascending=False, inplace=True)  # 排序
+
+df.sort_values(by='文件名', ascending=False, inplace=True)  # 排序
 df.index = range(file_num)  # 重新编号
-df['歌手重复'] = df.duplicated(subset='歌手', keep=False)
-df['歌名重复'] = df.duplicated(subset='歌名', keep=False)
-# df['重复文件'] = df.duplicated(subset='MD5', keep=False)
+df['重复文件'] = df.duplicated(subset='MD5', keep=False)
 df.to_excel(save_path)
