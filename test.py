0b8a70b5da944ed3dcd5970eee1e3dc2eac532fe
kevin <kevin_365@live.com>
2022-09-18 20:12:14 +0800

    2022-0918

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import os
+import sys
 import hashlib
 import pandas as pd
 
-base_dir = r'F:\手机文件整理\mate20\mate20音乐'  # 需要查找的根目录
-save_path = r'C:\Users\Kevin\Documents\文件信息.xlsx'  # 结果保存
+
+base_dir = r'F:\手机文件整理'  # 需要查找的根目录
+# base_dir = r'D:\照片'  # 需要查找的根目录
+save_path = r'C:\Users\Kevin\Documents\所有文件信息.xlsx'  # 结果保存
 df = pd.DataFrame(columns=('长文件名', '文件名', '后缀', '大小', 'MD5'))
 
 
@@ -18,6 +21,17 @@ def check_md5(file_name):
     return checksum.hexdigest()
 
 
+def get_dir_size(base_dir):  # 获取文件夹内文件大小
+    file_size = 0
+    for roots, dirs, file_names in os.walk(base_dir):
+        for name in file_names:
+            file_name = os.path.join(roots, name)  # 含路径的文件名
+            file_size += os.path.getsize(file_name)
+    return file_size
+
+
+total_size = get_dir_size(base_dir)
+file_sizes = 0
 file_num = 0
 for roots, dirs, file_names in os.walk(base_dir):
     for name in file_names:
@@ -28,6 +42,12 @@ for roots, dirs, file_names in os.walk(base_dir):
         df.loc[file_num] = file_name, name, file_suf, file_size, file_md5
         file_num += 1
 
+        file_sizes += file_size
+        if file_num % 5 == 0:
+            percent = int(file_sizes/total_size*100)
+            print('\r', end='')
+            print(percent, "%", '■'*percent, '>', file_num, end="")
+            sys.stdout.flush()
 
 df.sort_values(by='文件名', ascending=False, inplace=True)  # 排序
 df.index = range(file_num)  # 重新编号
--- 文件信息/文件目录遍历及MD5.py
+++ 文件信息/文件目录遍历及MD5.py
@@ -2,30 +2,35 @@ import os
 import hashlib
 import pandas as pd
 
-root_dir = r'D:\手机文档'  # 需要查找的根目录
-save_path = r'D:\python\data\所有文件信息MD5.xlsx'  # 结果保存
+base_dir = r'D:\杨芳文件夹'  # 需要查找的根目录
+save_path = r'C:\Users\Kevin\Documents\所有文件信息.xlsx'  # 结果保存
 df = pd.DataFrame(columns=('长文件名', '文件名', '后缀', '大小', 'MD5'))
 
 
-def check_md5(file):
+def check_md5(file_name):
     checksum = hashlib.md5()
-    with open(file, 'rb') as fp:
+    with open(file_name, 'rb') as fp:
         while True:
-            buffer = fp.read(65536)  # 计算MD5分块读MD，速度快
+            buffer = fp.read(51200)  # 分块读MD，速度快
             if not buffer:
                 break
             checksum.update(buffer)
     return checksum.hexdigest()
 
+
 file_num = 0
-for roots, dirs, files in os.walk(root_dir):
-    for name in files:
+for roots, dirs, file_names in os.walk(base_dir):
+    for name in file_names:
         file_name = os.path.join(roots, name)  # 含路径的文件名
         file_suf = os.path.splitext(name)[1]  # 文件后缀
         file_size = os.path.getsize(file_name)
         file_md5 = check_md5(file_name)
         df.loc[file_num] = file_name, name, file_suf, file_size, file_md5
         file_num += 1
+        if file_num % 200 == 0:  # 每200打印一次
+            print(file_num, file_name)
 
+df.sort_values(by='文件名', ascending=False, inplace=True)  # 排序
+df.index = range(file_num)  # 重新编号
 df['重复文件'] = df.duplicated(subset='MD5', keep=False)
 df.to_excel(save_path)
